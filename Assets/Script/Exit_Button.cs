0e12a1225ce541c972a9169ee844991cb477763a
songhyunji <37237094+songhyunji@users.noreply.github.com>
2018-04-10 14:00:27 +0900

    Change UI
    메인 화면 UI 수정했습니다! 어플리케이션 종료 버튼도 넣었어요!

---
+++ Assets/Script/Exit_Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit_Button : MonoBehaviour {
+
+    public GameObject exitButton;
+
+    /*// Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+                return;
+            }
+        }
+    }*/
+
+    public void ExitBtnPress()
+    {
+        Application.Quit();
+        Debug.LogError("Button Pressed");
+        return;
+    }
+
+}
---
+++ Assets/Script/Setting_Button_modified.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setting_Button_modified : MonoBehaviour {
+
+    public Image controlImage;
+    public Sprite[] controlSprite = new Sprite[2];
+
+    public GameObject settingPanel;
+
+    private bool easyControl;
+
+    public GameObject creditButton;
+    public GameObject creditPanel;
+
+    public void ControlBtnPress()
+    {
+        if (easyControl)
+        {
+            controlImage.sprite = controlSprite[0];
+        }
+        else
+        {
+            controlImage.sprite = controlSprite[1];
+        }
+
+        easyControl = !easyControl;
+    }
+
+    public void CreditBtnPress()
+    {
+        creditPanel.SetActive(true);
+    }
+
+    public void ExitCreditPress()
+    {
+        creditPanel.SetActive(false);
+    }
+
+}
