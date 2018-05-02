aae0ce96e3c93b1410c8af819d4da7655a7ef12a
songhyunji <37237094+songhyunji@users.noreply.github.com>
2018-05-02 13:02:39 +0900

    Add TeddyBear
    곰인형 + 말풍선 추가했습니다.

---
+++ Assets/Script/Teddy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teddy : MonoBehaviour {
+
+    public GameObject speechBubble;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        speechBubble.SetActive(true);
+    }
+}
