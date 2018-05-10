07ad408db3b68605a0aec3c536c6932b997cb4b6
songhyunji <37237094+songhyunji@users.noreply.github.com>
2018-05-10 17:16:57 +0900

    Rule Tile Stage 1, 2에 적용하였습니다.
    근데 타일 팔레트에 타일 이미지가 안보여서 (제컴퓨터에서 그러는건지 모르겠지만) 노트북으로 옮겨서 다시 확인해볼 예정입니닷

---
+++ Assets/Script/SpeechBubble.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour {
+
+    private Rigidbody2D rb2D;
+    public float jumpForce = 100;
+
+    public bool isTouching = false;
+  
+    void Start()
+    {
+        rb2D = this.gameObject.GetComponent<Rigidbody2D>();
+        Debug.Log("Start");
+    }
+
+    void FixedUpdate()
+    {
+        if (isTouching)
+        {
+
+            rb2D.AddForce(Vector2.up * jumpForce);
+            Debug.Log("jump");
+        }
+
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("speechBubble"))
+        {
+            isTouching = true;
+            Debug.Log("In");
+        }
+
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("speechBubble"))
+        {
+            isTouching = false;
+            Debug.Log("Out");
+        }
+
+    }
+
+}
