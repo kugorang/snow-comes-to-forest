41feeaec952ecbfbe17070f1dbb4cd08d077ff32
Kim Hyeonwoo <1reds@naver.com>
2018-05-27 10:47:09 +0900

    Add Stage2
    1. 가로등 기능 추가
    2. 발전기 기능 추가
    3. 곰돌이 눈알 기능 추가
    4. 움직이는 발판 추가

---
+++ Assets/Script/TurnOnOff.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnOff : MonoBehaviour
+{
+	private GameObject[] _streetLightOn;
+	private GameObject[] _streetLightOff;
+	private bool isTurnOn = false;
+
+	private void Awake()
+	{
+		_streetLightOn = GameObject.FindGameObjectsWithTag("lightOn");
+		_streetLightOff = GameObject.FindGameObjectsWithTag("lightOff");
+		isTurnOn = false;
+	}
+
+	// Use this for initialization
+	private void Start () 
+	{
+		foreach (var on in _streetLightOn)
+		{
+			on.SetActive(false);
+		}
+	}
+
+	public void On()
+	{
+		foreach (var on in _streetLightOn)
+		{
+			on.SetActive(true);
+		}
+
+		foreach (var off in _streetLightOff)
+		{
+			off.SetActive(false);
+		}
+
+		isTurnOn = true;
+	}
+
+	public void Off()
+	{
+		foreach (var on in _streetLightOn)
+		{
+			on.SetActive(false);
+		}
+
+		foreach (var off in _streetLightOff)
+		{
+			off.SetActive(true);
+		}
+
+		isTurnOn = false;
+	}
+}
