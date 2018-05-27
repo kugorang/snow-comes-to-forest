793e5b8780f1345f7cffc7c2a36e9a8e6393ceff
Koonwoo Yu <gunuboard@gmail.com>
2018-05-27 16:43:15 +0900

    Fix Meta File

--- Assets/Script/Stage1/Stage1.cs
+++ Assets/Script/Stage1/Stage1.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
